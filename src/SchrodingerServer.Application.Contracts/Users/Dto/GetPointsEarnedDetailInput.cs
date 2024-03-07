d55c8fa4d7f382aa485180940b89bc0273709e85
ferrariSte <ferrari_ste@schrodingernft.ai>
2024-03-07 20:28:12 +0800

    feat: remove dappid in my points interface

--- src/SchrodingerServer.Application.Contracts/Users/Dto/GetPointsEarnedDetailInput.cs
+++ src/SchrodingerServer.Application.Contracts/Users/Dto/GetPointsEarnedDetailInput.cs
@@ -4,7 +4,6 @@ namespace SchrodingerServer.Users.Dto;
 
 public class GetPointsEarnedDetailInput
 {
-    [Required] public string DappName { get; set; }
     [Required] public string Domain { get; set; }
     [Required] public string Address { get; set; }
 }

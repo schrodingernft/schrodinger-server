2259509735aadd2f040e2c80823011613cb04537
un2512 <u2520dr@schrodingernft.ai>
2024-03-16 14:57:20 +0800

    feat: add update repair xp score.

--- src/SchrodingerServer.Domain/Zealy/ZealyXpScoreIndex.cs
+++ src/SchrodingerServer.Domain/Zealy/ZealyXpScoreIndex.cs
@@ -7,6 +7,8 @@ namespace SchrodingerServer.Zealy;
 public class ZealyXpScoreIndex: SchrodingerEntity<string>, IIndexBuild
 {
     [Keyword] public override string Id { get; set; }
+    public decimal LastRawScore { get; set; }
+    public decimal LastActualScore { get; set; }
     public decimal RawScore { get; set; }
     public decimal ActualScore { get; set; }
     public long CreateTime { get; set; }

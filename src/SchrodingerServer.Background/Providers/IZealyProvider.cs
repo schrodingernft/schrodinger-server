76b2298877fcb38701fdc95dd4feb9f063a92059
un2512 <u2520dr@schrodingernft.ai>
2024-03-16 16:17:43 +0800

    modify: xp records.

--- src/SchrodingerServer.Background/Providers/IZealyProvider.cs
+++ src/SchrodingerServer.Background/Providers/IZealyProvider.cs
@@ -13,6 +13,8 @@ public interface IZealyProvider
     Task<List<ZealyUserIndex>> GetUsersAsync(int skipCount, int maxResultCount);
     Task<List<ZealyUserXpIndex>> GetUserXpsAsync(int skipCount, int maxResultCount);
     Task<List<ZealyXpScoreIndex>> GetXpScoresAsync(int skipCount, int maxResultCount);
+
+    Task<List<ZealyUserXpRecordIndex>> GetPendingUserXpsAsync(int skipCount, int maxResultCount);
 }
 
 public class ZealyProvider : IZealyProvider, ISingletonDependency
@@ -21,15 +23,18 @@ public class ZealyProvider : IZealyProvider, ISingletonDependency
     private readonly INESTRepository<ZealyUserIndex, string> _zealyUserRepository;
     private readonly INESTRepository<ZealyUserXpIndex, string> _zealyUserXpRepository;
     private readonly INESTRepository<ZealyXpScoreIndex, string> _zealyXpScoreRepository;
+    private readonly INESTRepository<ZealyUserXpRecordIndex, string> _zealyXpRecordRepository;
 
     public ZealyProvider(INESTRepository<ZealyUserIndex, string> zealyUserRepository, ILogger<ZealyProvider> logger,
         INESTRepository<ZealyUserXpIndex, string> zealyUserXpRepository,
-        INESTRepository<ZealyXpScoreIndex, string> zealyXpScoreRepository)
+        INESTRepository<ZealyXpScoreIndex, string> zealyXpScoreRepository,
+        INESTRepository<ZealyUserXpRecordIndex, string> zealyXpRecordRepository)
     {
         _zealyUserRepository = zealyUserRepository;
         _logger = logger;
         _zealyUserXpRepository = zealyUserXpRepository;
         _zealyXpScoreRepository = zealyXpScoreRepository;
+        _zealyXpRecordRepository = zealyXpRecordRepository;
     }
 
     public async Task<List<ZealyUserIndex>> GetUsersAsync(int skipCount, int maxResultCount)
@@ -52,6 +57,14 @@ public class ZealyProvider : IZealyProvider, ISingletonDependency
     {
         var (totalCount, data) =
             await _zealyXpScoreRepository.GetListAsync(skip: skipCount, limit: maxResultCount);
+
+        return data;
+    }
+
+    public async Task<List<ZealyUserXpRecordIndex>> GetPendingUserXpsAsync(int skipCount, int maxResultCount)
+    {
+        var (totalCount, data) =
+            await _zealyXpRecordRepository.GetListAsync(skip: skipCount, limit: maxResultCount);
 
         return data;
     }

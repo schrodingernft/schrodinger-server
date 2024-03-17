cb84f5e23bcfac083f64a4df30529b6597cf6b41
un2512 <u2520dr@schrodingernft.ai>
2024-03-17 17:20:43 +0800

    modify: update xp record period.

--- src/SchrodingerServer.Application.Contracts/Options/UpdateScoreOptions.cs
+++ src/SchrodingerServer.Application.Contracts/Options/UpdateScoreOptions.cs
@@ -3,4 +3,7 @@ namespace SchrodingerServer.Options;
 public class UpdateScoreOptions
 {
     public string RecurringCorn { get; set; } = "0 0 0 * * ?";
+    
+    // minutes
+    public int UpdateXpScoreResultPeriod { get; set; } = 20;
 }

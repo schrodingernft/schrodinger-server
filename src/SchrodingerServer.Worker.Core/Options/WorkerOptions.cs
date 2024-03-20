8335f0c333259d5ccd9f96d47588b069d80aba7d
so0o0o0up <siamese@schrodingernft.ai>
2024-03-20 18:34:46 +0800

    change grainId by option

--- src/SchrodingerServer.Worker.Core/Options/WorkerOptions.cs
+++ src/SchrodingerServer.Worker.Core/Options/WorkerOptions.cs
@@ -7,4 +7,6 @@ public class WorkerOptions
     public long BackFillBatchSize { get; set; } = 9999;
     public long SubscribeStartHeight { get; set; } = 108700000;
     public int MaximumNumberPerTask { get; set; } = 10;
+    public string SubscribeStartHeightGrainId { get; set; } = "StartHeight";
+    public string SyncPendingListGrainId { get; set; } = "SyncPendingList";
 }

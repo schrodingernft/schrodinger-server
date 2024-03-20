5a4efde05dc1bda5692a00c7f9964baed721eed8
un2512 <u2520dr@schrodingernft.ai>
2024-03-20 11:19:26 +0800

    modify: clean data.

--- src/SchrodingerServer.Background/Workers/UserRelationWorker.cs
+++ src/SchrodingerServer.Background/Workers/UserRelationWorker.cs
@@ -13,20 +13,23 @@ public class UserRelationWorker : AsyncPeriodicBackgroundWorkerBase
 {
     private readonly IUserRelationService _userRelationService;
     private readonly ILogger<UserRelationWorker> _logger;
+    private readonly IZealyScoreService _zealyScoreService;
 
     public UserRelationWorker(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory,
         IUserRelationService userRelationService, IOptionsSnapshot<ZealyUserOptions> options,
-        ILogger<UserRelationWorker> logger) : base(timer,
+        ILogger<UserRelationWorker> logger, IZealyScoreService zealyScoreService) : base(timer,
         serviceScopeFactory)
     {
         _userRelationService = userRelationService;
         _logger = logger;
+        _zealyScoreService = zealyScoreService;
         timer.Period = options.Value.Period * 60 * 1000;
     }
 
     protected override async Task DoWorkAsync(PeriodicBackgroundWorkerContext workerContext)
     {
         _logger.LogInformation("begin execute UserRelationWorker.");
+        await _zealyScoreService.UpdateScoreAsync();
        // await _userRelationService.AddUserRelationAsync();
         _logger.LogInformation("finish execute UserRelationWorker.");
     }

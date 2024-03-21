e7b5d7b634f83db3b9b8ba0285360e41770459b2
so0o0o0up <siamese@schrodingernft.ai>
2024-03-21 16:41:08 +0800

    Schedule tasks by random.

--- src/SchrodingerServer.Worker.Core/Options/WorkerOptions.cs
+++ src/SchrodingerServer.Worker.Core/Options/WorkerOptions.cs
@@ -5,8 +5,9 @@ public class WorkerOptions
     public int SearchTimer { get; set; } = 10;
     public string SyncSourceChainId { get; set; } = "tDVW";
     public long BackFillBatchSize { get; set; } = 9999;
-    public long SubscribeStartHeight { get; set; } = 108700000;
+    public long SubscribeStartHeight { get; set; } = 109103273;
     public int MaximumNumberPerTask { get; set; } = 10;
     public string SubscribeStartHeightGrainId { get; set; } = "StartHeight";
     public string SyncPendingListGrainId { get; set; } = "SyncPendingList";
+    public int SyncExecutionAccelerationThreshold { get; set; } = 100;
 }
